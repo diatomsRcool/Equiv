b10d6362438cdd7a7294acbbbb690fd6299cb873
Anne Thessen <annethessen@gmail.com>
2018-08-23 17:10:39 +1200

    returns list of tuples instead of list of lists

--- service.py
+++ service.py
@@ -1,11 +1,16 @@
 import json
-#import numpy as np
 import itertools
-#import jsoncompare
 
+#assuming search results are provided in json according to DATS
+#had to use a highly simplified, made up data set for development
+#this code loads the search results
 with open('search_results.json') as f:
     data = json.load(f)
 
+#this code creates a dictionary of results that incorporates the important elements
+#that are likely to be the same between equivalent data sets
+#the key is a number
+#the value is the list of important elements
 results = {}
 for i,d in enumerate(data):
 	m = []
@@ -17,9 +22,16 @@ for i,d in enumerate(data):
 	results[i] = m
 z = len(data)
 
+#create a list of the keys in the dictionary of results
+#this should be a list of numbers
 y = list(results.keys())
+
+#create a list of all possible pairwise combinations of the dictionary keys
+#all duplication is removed
 z = itertools.combinations(y,2)
 
+#using the list of all key combinations, compare the values
+#return a list of equivalent identifiers as tuples
 equiv = []
 for p,q in z:
 	print(p)
@@ -32,5 +44,5 @@ for p,q in z:
 	print(x)
 	if len(x) == len(a) - 1:
 		print(id_a + ' and ' + id_b + ' are equivalent data sets.')
-		equiv.append([id_a,id_b])
+		equiv.append((id_a,id_b))
 print(equiv)
